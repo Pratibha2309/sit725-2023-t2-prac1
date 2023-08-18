48c76c67708cfef0e1643d3a23da380ae606adca
Pratibha2309 <114346206+Pratibha2309@users.noreply.github.com>
2023-08-18 13:27:05 +0000

    a website to change the text

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeText()
+{
+    let potentialTexts = ['text1','text2','text3','text4','text5'];
+    let randomNumber = getRandomNumber(0,potentialTexts.length);
+    textToChangeTo = potentialTexts[randomNumber];
+    document.getElementById('header').innerHTML = textToChangeTo;
+}
+
+
+function getRandomNumber(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
